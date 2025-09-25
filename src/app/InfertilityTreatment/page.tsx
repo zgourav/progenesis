4490e0d9eb8883fe84cfe35fdb3486362dc1c259
zgourav <gourav.bhandari@zucolservices.com>
2025-09-25 11:20:18 +0530

    updated

---
+++ src/app/InfertilityTreatment/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-gray-50">
+      <div className="text-center p-8 rounded-2xl shadow-md bg-white">
+        <h1 className="text-3xl font-bold text-gray-900 mb-4">
+          Centers Comming Soon
+        </h1>
+        <p className="text-gray-600">
+          We are working hard to provide you with the best possible experience.
+        </p>
+      </div>
+    </main>
+  );
+}
